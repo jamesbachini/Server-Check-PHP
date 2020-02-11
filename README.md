1a16d5d6341cf629f38d3713b4a8aae4cb09e78a
James Bachini <james@jsecoin.com>
2020-02-11 13:26:48 +0000

    tidy up and added json output

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Server Check PHP
  PHP script for Ram and CPU usage stats for server health check
+
+For original blog post and more information visit: https://jamesbachini.com/ram-cpu-usage-php-script/
+
+## Updates
+
+Feb 2020 - added JSON output using servercheck.php?json=1
--- servercheck.php
+++ servercheck.php
@@ -1,46 +1,52 @@
 <?php
-$start_time = microtime(TRUE);
+  $start_time = microtime(TRUE);
 
-$free = shell_exec('free');
-$free = (string)trim($free);
-$free_arr = explode("\n", $free);
-$mem = explode(" ", $free_arr[1]);
-$mem = array_filter($mem, function($value) { return ($value !== null && $value !== false && $value !== ''); }); // removes nulls from array
-$mem = array_merge($mem); // puts arrays back to [0],[1],[2] after filter removes nulls
+  $free = shell_exec('free');
+  $free = (string)trim($free);
+  $free_arr = explode("\n", $free);
+  $mem = explode(" ", $free_arr[1]);
+  $mem = array_filter($mem, function($value) { return ($value !== null && $value !== false && $value !== ''); }); // removes nulls from array
+  $mem = array_merge($mem); // puts arrays back to [0],[1],[2] after filter removes nulls
 
-//print_r($mem); echo '<hr>';
-$memtotal = round($mem[1] / 1000000,2);
-$memused = round($mem[2] / 1000000,2);
-$memfree = round($mem[3] / 1000000,2);
+  //print_r($mem); echo '<hr>';
+  $memtotal = round($mem[1] / 1000000,2);
+  $memused = round($mem[2] / 1000000,2);
+  $memfree = round($mem[3] / 1000000,2);
 
-$membuffer = round($mem[5] / 1000000,2);
-$memcached = round($mem[6] / 1000000,2);
+  $memshared = round($mem[5] / 1000000,2);
+  $memcached = round($mem[6] / 1000000,2);
 
-$memusage = round(($memused - $memcached - $membuffer)/$memtotal*100,2);
+  $memusage = round(($memused - $memcached)/$memtotal*100,2);
 
-$connections = `netstat -ntu | grep :80 | grep ESTABLISHED | grep -v LISTEN | awk '{print $5}' | cut -d: -f1 | sort | uniq -c | sort -rn | grep -v 127.0.0.1 | wc -l`; 
-$totalconnections = `netstat -ntu | grep :80 | grep -v LISTEN | awk '{print $5}' | cut -d: -f1 | sort | uniq -c | sort -rn | grep -v 127.0.0.1 | wc -l`; 
+  $connections = `netstat -ntu | grep :80 | grep ESTABLISHED | grep -v LISTEN | awk '{print $5}' | cut -d: -f1 | sort | uniq -c | sort -rn | grep -v 127.0.0.1 | wc -l`; 
+  $totalconnections = `netstat -ntu | grep :80 | grep -v LISTEN | awk '{print $5}' | cut -d: -f1 | sort | uniq -c | sort -rn | grep -v 127.0.0.1 | wc -l`; 
 
-$load = sys_getloadavg();
-$cpuload = $load[0];
+  $load = sys_getloadavg();
+  $cpuload = $load[0];
 
-$diskfree = round(disk_free_space(".") / 1000000000);
-$disktotal = round(disk_total_space(".") / 1000000000);
-$diskused = round($disktotal - $diskfree);
+  $diskfree = round(disk_free_space(".") / 1000000000);
+  $disktotal = round(disk_total_space(".") / 1000000000);
+  $diskused = round($disktotal - $diskfree);
 
-$diskusage = round($diskused/$disktotal*100);
+  $diskusage = round($diskused/$disktotal*100);
 
-if ($memusage > 85 || $cpuload > 2 || $diskusage > 95) {
-	$trafficlight = 'red';
-} elseif ($memusage > 70 || $cpuload > 1 || $diskusage > 85) {
-	$trafficlight = 'orange';
-} else {
-	$trafficlight = '#2F2';
-}
+  if ($memusage > 85 || $cpuload > 2 || $diskusage > 95) {
+    $trafficlight = 'red';
+  } elseif ($memusage > 70 || $cpuload > 1 || $diskusage > 85) {
+    $trafficlight = 'orange';
+  } else {
+    $trafficlight = '#2F2';
+  }
 
-$end_time = microtime(TRUE);
-$time_taken = $end_time - $start_time;
-$total_time = round($time_taken,4);
+  $end_time = microtime(TRUE);
+  $time_taken = $end_time - $start_time;
+  $total_time = round($time_taken,4);
+
+  // use servercheck.php?json=1
+  if (isset($_GET['json'])) {
+    echo '{"ram":'.$memusage.',"cpu":'.$cpuload.',"disk":'.$diskusage.',"connections":'.$totalconnections.'}';
+    exit;
+  }
 
 ?>
 <html>
@@ -53,14 +59,13 @@ body{ font-family: Arial,sans-serif; margin: 0px; }
 #details { font-size: 0.8em; }
 hr {  border: 0; height: 1px; background-image: linear-gradient(to right, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0)); }
 .big { font-size: 1.2em; }
-.footer { font-size: 0.7em; font-color: #AAA; text-align: center; }
+.footer { font-size: 0.5em; font-color: #AAA; text-align: center; }
 .footer a { font-color: #AAA; }
 .footer a:visited { font-color: #AAA; }
-
 </style>
 <body>
-<div id="container">
-<canvas id="trafficlight" width="100" height="100"></canvas>
+  <div id="container">
+    <canvas id="trafficlight" width="100" height="100"></canvas>
     <script>
       var canvas = document.getElementById('trafficlight');
       var context = canvas.getContext('2d');
@@ -76,30 +81,30 @@ hr {  border: 0; height: 1px; background-image: linear-gradient(to right, rgba(0
       context.strokeStyle = '#003300';
       context.stroke();
     </script>
-<p><span class="description big">RAM Usage:</span> <span class="result big"><?php echo $memusage; ?>%</span></p>
-<p><span class="description big">CPU Usage: </span> <span class="result big"><?php echo $cpuload; ?>%</span></p>
-<p><span class="description">Hard Disk Usage: </span> <span class="result"><?php echo $diskusage; ?>%</span></p>
-<p><span class="description">Established Connections: </span> <span class="result"><?php echo $connections; ?></span></p>
-<p><span class="description">Total Connections: </span> <span class="result"><?php echo $totalconnections; ?></span></p>
 
-<hr>
-<p><span class="description">RAM Total:</span> <span class="result"><?php echo $memtotal; ?> GB</span></p>
-<p><span class="description">RAM Free:</span> <span class="result"><?php echo $memfree; ?> GB</span></p>
-<p><span class="description">RAM Used:</span> <span class="result"><?php echo $memused; ?> GB</span></p>
-<p><span class="description">RAM Buffer:</span> <span class="result"><?php echo $membuffer; ?> GB</span></p>
-<p><span class="description">RAM Cached:</span> <span class="result"><?php echo $memcached; ?> GB</span></p>
-<hr>
-<p><span class="description">Hard Disk Free:</span> <span class="result"><?php echo $diskfree; ?> GB</span></p>
-<p><span class="description">Hard Disk Used:</span> <span class="result"><?php echo $diskused; ?> GB</span></p>
-<p><span class="description">Hard Disk Total:</span> <span class="result"><?php echo $disktotal; ?> GB</span></p>
-<hr>
-	<div id="details">
-		<p><span class="description">Server Name: </span> <span class="result"><?php echo $_SERVER['SERVER_NAME']; ?></span></p>
-		<p><span class="description">Server Addr: </span> <span class="result"><?php echo $_SERVER['SERVER_ADDR']; ?></span></p>
-		<p><span class="description">PHP Version: </span> <span class="result"><?php echo phpversion(); ?></span></p>
-		<p><span class="description">Load Time: </span> <span class="result"><?php echo $total_time; ?> sec</span></p>
-	</div>
-</div>
-<div class="footer">&copy; Copyright 2016 <a href="http://jim.am">Jim.am</a></div>
+    <p><span class="description big">RAM Usage:</span> <span class="result big"><?php echo $memusage; ?>%</span></p>
+    <p><span class="description big">CPU Usage: </span> <span class="result big"><?php echo $cpuload; ?>%</span></p>
+    <p><span class="description">Hard Disk Usage: </span> <span class="result"><?php echo $diskusage; ?>%</span></p>
+    <p><span class="description">Established Connections: </span> <span class="result"><?php echo $connections; ?></span></p>
+    <p><span class="description">Total Connections: </span> <span class="result"><?php echo $totalconnections; ?></span></p>
+    <hr>
+    <p><span class="description">RAM Total:</span> <span class="result"><?php echo $memtotal; ?> GB</span></p>
+    <p><span class="description">RAM Free:</span> <span class="result"><?php echo $memfree; ?> GB</span></p>
+    <p><span class="description">RAM Used:</span> <span class="result"><?php echo $memused; ?> GB</span></p>
+    <p><span class="description">RAM Shared:</span> <span class="result"><?php echo $memshared; ?> GB</span></p>
+    <p><span class="description">RAM Cached:</span> <span class="result"><?php echo $memcached; ?> GB</span></p>
+    <hr>
+    <p><span class="description">Hard Disk Free:</span> <span class="result"><?php echo $diskfree; ?> GB</span></p>
+    <p><span class="description">Hard Disk Used:</span> <span class="result"><?php echo $diskused; ?> GB</span></p>
+    <p><span class="description">Hard Disk Total:</span> <span class="result"><?php echo $disktotal; ?> GB</span></p>
+    <hr>
+    <div id="details">
+      <p><span class="description">Server Name: </span> <span class="result"><?php echo $_SERVER['SERVER_NAME']; ?></span></p>
+      <p><span class="description">Server Addr: </span> <span class="result"><?php echo $_SERVER['SERVER_ADDR']; ?></span></p>
+      <p><span class="description">PHP Version: </span> <span class="result"><?php echo phpversion(); ?></span></p>
+      <p><span class="description">Load Time: </span> <span class="result"><?php echo $total_time; ?> sec</span></p>
+    </div>
+  </div>
+  <div class="footer">&copy; Copyright 2020 <a href="https://jamesbachini.com">JamesBachini.com</a></div>
 </body>
 </html>
